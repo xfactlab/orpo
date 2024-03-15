38863694b3acc377032304628785d7c535af9444
jiwooya1000 <jiwooya1000@gmail.com>
2024-03-16 02:35:22 +0900

    Add dataset split generator

--- src/utils.py
+++ src/utils.py
@@ -3,3 +3,6 @@ def preprocess_logits_for_metrics(logits, labels):
     if isinstance(logits, tuple):
         logits = logits[0]
     return logits.argmax(dim=-1)
+
+def dataset_split_selector(data_name, data):
+    return None
