98a52218ba182e27f25c20fadc1385721146c4fc
jiwooya1000 <jiwooya1000@gmail.com>
2024-03-16 13:12:55 +0900

    Support other preference datasets

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,20 @@
 
+from typing import List
+
 def preprocess_logits_for_metrics(logits, labels):
     if isinstance(logits, tuple):
         logits = logits[0]
     return logits.argmax(dim=-1)
 
-def dataset_split_selector(data_name, data):
-    return None
+def dataset_split_selector(data) -> List:
+    """
+    This is a function for automating the process of selecting data split.
+    Will be further updated.
+    """
+    if len(data.keys()) == 1:
+        return ['train']
+    else:
+        if 'train_prefs' in data.keys():
+            return ['train_prefs', 'test_prefs']
+        else:
+            return ['train', 'test']
