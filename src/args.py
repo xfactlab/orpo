88e7cf2c0fce747712bfce2861ddb4a783c39eca
javafa <kodonho@gmail.com>
2024-04-16 15:33:44 +0900

    Added quantization default values

--- src/args.py
+++ src/args.py
@@ -1,8 +1,10 @@
+import torch
+
 def default_args(parser):
     parser.add_argument("--cache_dir", default=None, type=str)
-    parser.add_argument("--save_dir", default='./saved', type=str)
-    parser.add_argument("--data_name", default='HuggingfaceH4/UltraFeedback', type=str)
-    parser.add_argument("--model_name", default="gpt2", type=str)
+    parser.add_argument("--save_dir", default='./output', type=str)
+    parser.add_argument("--data_name", default='freewheelin/common_gen_dpo', type=str)
+    parser.add_argument("--model_name", default="hwkwon/S-SOLAR-10.7B-v1.5", type=str)
 
     # Training Arguments
     parser.add_argument("--torch_compile", default=False, type=bool)
@@ -11,10 +13,10 @@ def default_args(parser):
     parser.add_argument("--optim", default="paged_adamw_32bit", type=str)
     parser.add_argument("--overwrite_output_dir", default=True, type=bool)
     parser.add_argument("--lr", default=2e-5, type=float)
-    parser.add_argument("--num_proc", default=8, type=int)
-    parser.add_argument("--num_train_epochs", default=10, type=int)
-    parser.add_argument("--per_device_train_batch_size", default=2, type=int)
-    parser.add_argument("--per_device_eval_batch_size", default=2, type=int)
+    parser.add_argument("--num_proc", default=2, type=int)
+    parser.add_argument("--num_train_epochs", default=2, type=int)
+    parser.add_argument("--per_device_train_batch_size", default=1, type=int)
+    parser.add_argument("--per_device_eval_batch_size", default=1, type=int)
     parser.add_argument("--warmup_steps", default=5000, type=int)    
     parser.add_argument("--evaluation_strategy", default='epoch', type=str)
     parser.add_argument("--do_eval", action='store_true')
@@ -36,7 +38,7 @@ def default_args(parser):
     parser.add_argument("--quantization_bit", default=4)
     parser.add_argument("--quantization_type", default='nf4')
     parser.add_argument("--double_quantization", default=True)
-    parser.add_argument("--compute_dtype", action='set in main.py > args.compute_dtype = torch.bfloat16')
+    parser.add_argument("--compute_dtype", default=torch.bfloat16)
 
     # Wandb Configurations
     parser.add_argument("--wandb_entity", default=None, type=str)
